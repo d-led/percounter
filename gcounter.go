87577b55c901c24351839ce943f691645be4df3c
Dmitry Ledentsov <dmlled@yahoo.com>
2024-06-15 11:06:37 +0200

    merge test

--- gcounter.go
+++ gcounter.go
@@ -1,5 +1,7 @@
 package percounter
 
+import "math"
+
 type GCounter struct {
 	identity string
 	state    GCounterState
@@ -17,14 +19,34 @@ func NewGCounter(identity string) *GCounter {
 }
 
 func (c *GCounter) Increment() {
-	val := c.state.Peers[c.identity]
+	val := c.valueOf(c.identity)
 	val++
-	c.state.Peers[c.identity] = val
+	c.setValueOf(c.identity, val)
 }
 
 func (c *GCounter) Value() int64 {
-	if val, ok := c.state.Peers[c.identity]; ok {
+	var res int64
+	for _, value := range c.state.Peers {
+		res += value
+	}
+	return res
+}
+
+func (c *GCounter) MergeWith(other *GCounter) {
+	for peer, value := range other.state.Peers {
+		myValue := c.valueOf(peer)
+		newValue := int64(math.Max(float64(value), float64(myValue)))
+		c.setValueOf(peer, newValue)
+	}
+}
+
+func (c *GCounter) valueOf(peer string) int64 {
+	if val, ok := c.state.Peers[peer]; ok {
 		return val
 	}
 	return 0
+}
+
+func (c *GCounter) setValueOf(peer string, val int64) {
+	c.state.Peers[peer] = val
 }
--- gcounter_test.go
+++ gcounter_test.go
@@ -14,4 +14,14 @@ func TestGCounter(t *testing.T) {
 		c.Increment()
 		assert.Equal(t, int64(3), c.Value())
 	})
+
+	t.Run("merging with another counter", func(t *testing.T) {
+		c := NewGCounter("1")
+		c.Increment()
+		c.Increment()
+		c2 := NewGCounter("2")
+		c2.Increment()
+		c.MergeWith(c2)
+		assert.Equal(t, int64(3), c.Value())
+	})
 }
