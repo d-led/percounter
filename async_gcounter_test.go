dffb601c28ea2636e812f8f5d8269f3586636b35
Dmitry Ledentsov <dmlled@yahoo.com>
2024-06-15 11:27:25 +0200

    eventual consistency in-process

--- async_gcounter_test.go
+++ async_gcounter_test.go
@@ -1,7 +1,9 @@
 package percounter
 
 import (
+	"log"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -13,5 +15,27 @@ func TestAsyncGCounter(t *testing.T) {
 		c.Increment()
 		c.Increment()
 		assert.Equal(t, int64(3), c.Value())
+	})
+
+	t.Run("eventual consistency in-process", func(t *testing.T) {
+		c := NewAsyncGCounter("1")
+		const goroutineCount = 32
+		const incrementCount = 1000
+		for i := 0; i < goroutineCount; i++ {
+			go func() {
+				for j := 0; j < incrementCount; j++ {
+					c.Increment()
+				}
+			}()
+		}
+		for w := 0; w < 15; w++ {
+			if goroutineCount*incrementCount == c.Value() {
+				// all ok
+				return
+			}
+			log.Println("waiting for the counter to arrive at the expected value...")
+			time.Sleep(100 * time.Millisecond)
+		}
+		assert.Equal(t, int64(goroutineCount*incrementCount), c.Value())
 	})
 }
