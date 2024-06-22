58b05b6085906a6821595dad159d736f47454e76
Dmitry Ledentsov <dmlled@yahoo.com>
2024-06-22 14:28:32 +0200

    generalization of the zeromq cluster

---
+++ zmq_cluster.go
@@ -0,0 +1,164 @@
+package percounter
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/Arceliar/phony"
+	"github.com/go-zeromq/zmq4"
+)
+
+type ClusterListener interface {
+	OnMessage(message []byte)
+	OnNewPeerConnected(c Cluster, peer string)
+}
+
+type Cluster interface {
+	UpdatePeers(peers []string)
+	BroadcastMessage(message []byte)
+}
+
+type ZmqCluster struct {
+	phony.Inbox
+	listener          ClusterListener
+	bindAddr          string
+	server            zmq4.Socket
+	peers             map[string]zmq4.Socket
+	started           bool
+	ctx               context.Context
+	stop              context.CancelFunc
+	receiveLaterDelay time.Duration
+	myIdentity        string
+}
+
+func NewZmqCluster(identity, bindAddr string, listener ClusterListener) *ZmqCluster {
+	ctx, cancel := context.WithCancel(context.Background())
+	res := &ZmqCluster{
+		bindAddr:          bindAddr,
+		listener:          listener,
+		server:            zmq4.NewPull(ctx),
+		peers:             make(map[string]zmq4.Socket),
+		ctx:               ctx,
+		stop:              cancel,
+		myIdentity:        identity,
+		receiveLaterDelay: 100 * time.Millisecond,
+	}
+	return res
+}
+
+func (z *ZmqCluster) Start() error {
+	var err error
+	phony.Block(z, func() {
+		if z.started {
+			err = errors.New("already started")
+		}
+		err = z.server.Listen(z.bindAddr)
+		if err != nil {
+			err = fmt.Errorf("could not start listening at %s: %v", z.bindAddr, err)
+			return
+		}
+		z.started = true
+		go z.receiveLoop()
+	})
+	return err
+}
+
+func (z *ZmqCluster) Stop() {
+	// safe to call from any goroutine
+	z.stop()
+	// this must be synchronized
+	phony.Block(z, func() {
+
+		log.Println("disconnecting", z.myIdentity)
+		z.server.Close()
+	})
+}
+
+// UpdatePeers adds new connections and removes ones not present in the input
+func (z *ZmqCluster) UpdatePeers(peers []string) {
+	z.Act(z, func() {
+		newPeers := setOf(peers)
+
+		// if not in new peers, close & remove the connection
+		for clientPeer, conn := range z.peers {
+			if _, ok := newPeers[clientPeer]; !ok {
+				log.Printf("%s: removing connection to %s", z.myIdentity, clientPeer)
+				conn.Close()
+				delete(z.peers, clientPeer)
+			}
+		}
+
+		// if not connected, connect
+		for _, peer := range peers {
+			if _, ok := z.peers[peer]; !ok {
+				socket := zmq4.NewPush(context.Background())
+				log.Printf("%s: connecting to %s", z.myIdentity, peer)
+				err := socket.Dial(peer)
+				if err != nil {
+					log.Printf("%s: could not connect to peer: %s: %v", z.myIdentity, peer, err)
+					continue
+				}
+				z.peers[peer] = socket
+				z.listener.OnNewPeerConnected(z, peer)
+			}
+		}
+	})
+}
+
+func (z *ZmqCluster) BroadcastMessage(message []byte) {
+	z.Act(z, func() {
+		if len(z.peers) == 0 {
+			log.Printf("%s: no peers in cluster of '%s', ignoring message", z.myIdentity, z.myIdentity)
+			return
+		}
+
+		for _, client := range z.peers {
+			err := client.Send(zmq4.NewMsg(message))
+			if err != nil {
+				log.Printf("%s: error sending state state: %v", z.myIdentity, err)
+				continue
+			}
+		}
+	})
+}
+
+func (z *ZmqCluster) receiveLoop() {
+	log.Printf("%s: started listening to incoming messages at %s", z.myIdentity, z.bindAddr)
+	for {
+		select {
+		case <-z.ctx.Done():
+			log.Printf("%s: stopped listening to incoming messages at %s", z.myIdentity, z.bindAddr)
+			z.started = false
+			return
+		default:
+		}
+
+		msg, err := z.server.Recv()
+		if err != nil {
+			log.Printf("%s: failed receive: %v", z.myIdentity, err)
+			if !errors.Is(err, context.Canceled) {
+				go z.receiveLater()
+			} else {
+				log.Printf("%s: stopping receive loop", z.myIdentity)
+			}
+			return
+		}
+		z.listener.OnMessage(msg.Bytes())
+	}
+}
+
+func (z *ZmqCluster) receiveLater() {
+	time.Sleep(z.receiveLaterDelay)
+	z.receiveLoop()
+}
+
+func setOf(s []string) map[string]bool {
+	var res = make(map[string]bool)
+	for _, e := range s {
+		res[e] = true
+	}
+	return res
+}
